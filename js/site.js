0f8963d76427349e420dc9eb859ae3bdfa66d518
Robson Soares <robson_drs@hotmail.com>
2020-10-03 23:59:18 -0300

    add animaçoes

--- js/site.js
+++ js/site.js
@@ -7,13 +7,41 @@ $(window).scroll(function(){
     }
 });
 
-//Ancoras
-$('#navegacao a').click(function(){
-    if($($(this).attr('href')).length > 0){
-        var top = $($(this).attr('href')).offset().top;
-        $('html,body').animate({
-            scrollTop: top
-        });
-        return false;
+$(document).ready(function(){
+    if($(window).scrollTop() > 0){
+        $('#navegacao').addClass('scroll');
     }
+})
+
+/*-- ANIMAÇÃO COM SCROLL PARA CARREGAMENTO DE BLOCOS --*/
+
+var root = document.documentElement;
+root.className += ' js';
+
+function boxTop(idBox) {
+  var boxOffset = $(idBox).offset().top;
+  return boxOffset;
+}
+
+$(document).ready(function() {
+  var $target = $(".anime-esquerda , .anime-topo , .anime-direita , .anime-debaixo"),
+      animationClass = 'anime-init',
+      windowHeight = $(window).height(),
+      offset = windowHeight - (windowHeight / 4);
+
+  function animeScroll() {
+    var documentTop = $(document).scrollTop();
+    $target.each(function() {
+      if (documentTop > boxTop(this) - offset) {
+        $(this).addClass(animationClass);
+      } else {
+        $(this).removeClass(animationClass);
+      }
+    });
+  }
+  animeScroll();
+
+  $(document).scroll(function() {
+    animeScroll();
+  });
 });
